e64d72de360d3b1662df26f3aafacf947cf1ed9f
Summer Green <854232507@qq.com>
2016-06-20 18:42:55 +0800

    better the name

--- testglobalenv.py
+++ testglobalenv.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python
 
+# need the dependency of matplotlib
+
 import matplotlib.pyplot as plt
 import re
 
-def getEnvPara(testlog):
-    obstacles = []
-    size = None
-    try :
-        f = open(testlog, 'r')
-        for eachLine in f.readlines():
-            matchSize = re.match('PrintEnvironment : .*length (\d+).*width (\d+)', eachLine)
-            if matchSize:
-                size = tuple([int(i) for i in matchSize.groups()])
-            matchObstacle = re.match('PrintEnvironmentMember :.*x (\d+).*y (\d+).*with obstacle$', eachLine)
-            if matchObstacle:
-                nums = matchObstacle.groups()
+def get_env_para(testlog):
+    try:
+        fobj = open(testlog, 'r')
+        obstacles = []
+        size = None
+        for each_line in fobj.readlines():
+            match_size = re.match('PrintEnvironment : .*length (\\d+).*width (\\d+)', each_line)
+            if match_size:
+                size = tuple([int(i) for i in match_size.groups()])
+            match_obstacle = re.match('PrintEnvironmentMember :.*x (\\d+).*y (\\d+).*with obstacle$', each_line)
+            if match_obstacle:
+                nums = match_obstacle.groups()
                 obstacles.append(nums)
-            f.close()
-    except IOError, e:
-        print 'File not found :', e
+            fobj.close()
+    except IOError, error_msg:
+        print 'File not found :', error_msg
     return (size, obstacles)
 
-def plotEnv(size, obstacles):
+def plot_env(size, obstacles):
     env_scatter = plt.subplot(1,1,1)
     length, width = size
     env_scatter.set_xlim([0, length])
@@ -32,5 +34,5 @@ def plotEnv(size, obstacles):
     plt.show()
 
 if __name__ == '__main__':
-    size, obstacles = getEnvPara('test.log')
-    plotEnv(size, obstacles)
+    size, obstacles = get_env_para('test.log')
+    plot_env(size, obstacles)
